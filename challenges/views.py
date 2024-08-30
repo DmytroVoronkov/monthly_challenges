d65b0f253055123a129fc05c126ee7964e8b3571
Dmytró Voronkov <dmvr2107@gmail.com>
2024-08-30 10:41:02 +0300

    + route for every month

--- challenges/views.py
+++ challenges/views.py
@@ -1,14 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 
-# Create your views here.
-def index(request):
-    return HttpResponse("Hello Django!")
+monthly_challenges = {    
+    'january': 'Corporis cupiditate fugit deserunt repellendus.',
+    'february': 'Accusamus hic dolor.',
+    'march': 'Aut et voluptate voluptatum ab sed magni quos.',
+    'april': 'Dolor maxime cupiditate occaecati qui.',
+    'may': 'Nobis iure est odio.',
+    'june': 'Eum qui facere autem.',
+    'july': 'Eum atque earum error autem harum cum est et autem.',
+    'august': 'Repudiandae ea unde odio porro velit ipsum atque.',
+    'september': 'Rem reprehenderit adipisci doloremque est quibusdam repellat iure.',
+    'october': 'Reprehenderit totam sunt quam inventore ut minima aut.',
+    'november': 'Et dignissimos velit ipsum quia.',
+    'dcember': 'Numquam molestiae sed.'
+}
 
-def monthly_challenge(request, month):
-    challenge_text = None
-    if month == 'january':
-        challenge_text = "Run every day"
-    else:
-        return HttpResponseNotFound("No challenge this month")
+
+# Create your views here.
+def monthly_challenge(request, month: str):
+    if month not in monthly_challenges.keys():
+        return HttpResponseNotFound("Invalid month")
+    
+    challenge_text = monthly_challenges[month]
     return HttpResponse(challenge_text)
