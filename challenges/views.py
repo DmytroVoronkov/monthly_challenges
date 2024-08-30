6eb8f60fc4c403cc647f5ff5df56026af76a3783
Dmytró Voronkov <dmvr2107@gmail.com>
2024-08-30 10:11:30 +0300

    + dynamic path segments

--- challenges/views.py
+++ challenges/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello Django!")
+
+def monthly_challenge(request, month):
+    challenge_text = None
+    if month == 'january':
+        challenge_text = "Run every day"
+    else:
+        return HttpResponseNotFound("No challenge this month")
+    return HttpResponse(challenge_text)
