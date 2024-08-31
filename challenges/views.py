4d66ab0272a7dbfc58637cca3d59f930b1c33a43
Dmytró Voronkov <dmvr2107@gmail.com>
2024-08-31 14:53:32 +0300

    ~ changed render function

--- challenges/views.py
+++ challenges/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
-from django.template.loader import render_to_string
+# from django.template.loader import render_to_string
 
 
 monthly_challenges = {    
@@ -31,11 +31,8 @@ def index(request):
 
 def monthly_challenge(request, month: str):
     try:
-    
-        challenge_text = monthly_challenges[month]
-        # response_data = f"<h1>{challenge_text}</h1>"
-        response_data = render_to_string('challenges/challenge.html')
-        return HttpResponse(response_data)
+        # challenge_text = monthly_challenges[month]
+        return render(request, 'challenges/challenge.html')
     except:
         return HttpResponseNotFound("<h1>This month is not supported</h1>")
 
